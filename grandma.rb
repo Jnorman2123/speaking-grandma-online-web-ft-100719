86bce06ae586ef76a686f55523d0a5ef8a7a7540
Jonathan Norman <jnorman2123@gmail.com>
2019-10-07 22:21:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_gradma(string)
   if string == "I LOVE YOU GRANDMA!"
     p "I LOVE YOU TOO PUMPKIN!"
-  end 
+  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
