0cdb6fe2cd675f781f35420227e4a77dea9e9cf4
Jonathan Norman <jnorman2123@gmail.com>
2019-10-07 22:21:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,13 @@
 # Write a speak_to_grandma method.
-def speak_to_gradma(string) 
+def speak_to_gradma(string)
   if string == "I LOVE YOU GRANDMA!"
     p "I LOVE YOU TOO PUMPKIN!"
-end 
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
