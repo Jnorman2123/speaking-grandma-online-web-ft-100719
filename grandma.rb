ebb1c5462265fd96db310a07adbfc1152b3252ea
Jonathan Norman <jnorman2123@gmail.com>
2019-10-07 22:26:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string == "I LOVE YOU GRANDMA!"
-    p "I LOVE YOU TOO PUMPKIN!"
+  if string != string.upcase
+    p "HUH?! SPEAK UP, SONNY!"
+  elsif string == string.upcase
+    p "NO, NOT SINCE 1938!"
+  elsif string == "I LOVE YOU GRANDMA!"
+    p "I LOVE YOU TOO PUMPKIN!" 
   end
 end
 # Whatever you say to grandma, she should respond with
